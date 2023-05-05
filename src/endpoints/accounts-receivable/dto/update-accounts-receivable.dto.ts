12bfcb31bf63a0f5a7260770b4d675cf98e506b1
devLopesRenato <renatolopes37@gmail.com>
2023-05-05 00:49:16 -0300

    feat: Adicionado validações de parametros de entrada

--- src/endpoints/accounts-receivable/dto/update-accounts-receivable.dto.ts
+++ src/endpoints/accounts-receivable/dto/update-accounts-receivable.dto.ts
@@ -1,11 +1,14 @@
 
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 
 export class UpdateAccountsReceivableDto {
     @ApiProperty({ type: 'string', description: 'Descrição da conta a receber.' })
+    @IsString()
     description: string;
 
     @ApiProperty({ type: 'number', description: 'Valor da conta a receber.' })
+    @IsNumber()
     value: number;
 }
